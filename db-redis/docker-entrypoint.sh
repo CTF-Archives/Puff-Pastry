c790469d086eb230d2853cd6b1c269d1ad4ee15c
Randark <randark@randark.site>
2026-06-02 02:17:41 +0000

    优化docker-entry点脚本以确保Redis启动完成后再执行命令

--- db-redis/docker-entrypoint.sh
+++ db-redis/docker-entrypoint.sh
@@ -2,6 +2,14 @@
 
 rm -f /docker-entrypoint.sh
 
-redis-server --save 20 1 --loglevel warning --requirepass 12345
+redis-server --save 20 1 --loglevel warning --requirepass 12345 &
+
+until redis-cli -a 12345 ping >/dev/null 2>&1
+do
+  echo "waiting for redis ..."
+  sleep 1
+done
 
 redis-cli -a 12345 SET flag "$(cat /flag.txt)"
+
+wait
